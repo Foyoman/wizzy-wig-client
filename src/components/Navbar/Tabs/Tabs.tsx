9af99a33e11ffba1df93f097f7b756989931a9b0
Edward Do <edwardtdo@gmail.com>
2023-03-17 12:46:58 +1100

    feat: tab bar overflow styling and functionality

--- src/components/Navbar/Tabs/Tabs.tsx
+++ src/components/Navbar/Tabs/Tabs.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Tabs.scss';
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
@@ -12,7 +12,14 @@ export default function Tabs() {
 	const dispatch = useDispatch();
 	const tabs = useSelector((state: RootState) => state.app.tabs);
 	const selectedTab = useSelector((state: RootState) => state.app.selectedTab);
-	
+	const tabBarEl: React.MutableRefObject<HTMLDivElement | null> = useRef(null);
+
+	// scroll to end of tab bar if new tab is created
+	useEffect(() => {
+		tabBarEl.current!.scrollLeft = 
+			tabBarEl.current!.scrollWidth - tabBarEl.current!.offsetWidth;
+	}, [tabs.length])
+
 	interface TabProps {
 		fsFile?: FsFile;
 		title?: string;
@@ -40,7 +47,11 @@ export default function Tabs() {
 		return (
 			<div 
 				onClick={() => handleSelect(index)} 
-				className={`tab  ${ selected ? 'selected' : ''}`} 
+				className={`
+					tab  
+					${ selected ? 'selected' : ''}
+					${ index >= tabs.length - 1 ? 'last-index' : '' }
+				`} 
 				title={title}
 			>
 				<p>{ title }</p>
@@ -50,30 +61,37 @@ export default function Tabs() {
 	}
 
 	return (
-		<div className='tab-bar'>
-			<div className='tabs'>
-			{tabs.map((file, index) => {
-				if (file) {
-					return (
-						<Tab 
-							fsFile={file} 
-							title={file.title} 
-							selected={index === selectedTab} 
-							index={index}
-						/>
-					)
-				} else {
-					return (
-						<Tab 
-							title="New tab" 
-							selected={index === selectedTab} 
-							index={index} 
-						/>
-					)
-				}
-			})}
+		<div className="tab-container">
+			<div className='tab-bar' ref={tabBarEl}>
+				<div className='tabs'>
+				{tabs.map((file, index) => {
+					if (file) {
+						return (
+							<Tab 
+								fsFile={file} 
+								title={file.title} 
+								selected={index === selectedTab} 
+								index={index}
+							/>
+						)
+					} else {
+						return (
+							<Tab 
+								title="New tab" 
+								selected={index === selectedTab} 
+								index={index} 
+							/>
+						)
+					}
+				})}
+				</div>
 			</div>
-			<AddOutlinedIcon className='new-tab-icon' onClick={() => dispatch(newTab(""))} />
+			<div className="new-tab">
+				<AddOutlinedIcon 
+					className='new-tab-icon' 
+					onClick={() => dispatch(newTab(""))} 
+				/>
+			</div>
 		</div>
 	)
 }
