d1862f06b9b8584b1af74d3ccdb25269fff66715
Edward Do <edwardtdo@gmail.com>
2023-04-06 15:45:09 -0600

    updating count api

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -69,21 +69,36 @@ export default function App() {
   useEffect(() => {
     const countKey = process.env.REACT_APP_COUNT_KEY as string;
 
+    const getOrdinal = (n: number) => {
+      let ord = 'th';
+    
+      if (n % 10 === 1 && n % 100 !== 11) {
+        ord = 'st';
+      } else if (n % 10 === 2 && n % 100 !== 12) {
+        ord = 'nd';
+      } else if (n % 10 === 3 && n % 100 !== 13) {
+        ord = 'rd';
+      }
+    
+      return String(n) + ord;
+    }
+
     if (localStorage.getItem('status') !== 'visited') {
       countapi.update('wizzy-wig.netlify.app', countKey, 1)
         .then((result) => {
-          console.log(result);
+          const ordinal = getOrdinal(result.value);
+          console.log(`congratulations, you are the ${ordinal} unique visitor`);
+        }
+      )
+    } else {
+      countapi.get('wizzy-wig.netlify.app', countKey)
+        .then((result) => {
+          console.log(`this page has had ${result.value} unique visit${result.value > 1 ? 's' : ''}`);
         }
       )
     }
 
     localStorage.setItem('status', 'visited');
-
-    countapi.get('wizzy-wig.netlify.app', countKey)
-      .then((result) => {
-        console.log(result);
-      }
-    )
   }, [])
   
   const handleClick = (e: Event) => {
