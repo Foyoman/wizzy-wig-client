e8e56c40882315e6656f06c8ea74a6c538773f96
Edward Do <edwardtdo@gmail.com>
2023-06-16 17:22:51 -0600

    removed visit counter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -65,49 +65,6 @@ export default function App() {
     setContent(selectedFile?.content as string | undefined)
   }, [selectedFile]);
 
-  // count page visits
-  useEffect(() => {
-    const countKey = process.env.REACT_APP_COUNT_KEY as string;
-
-    const getOrdinal = (n: number) => {
-      let ord: 'th' | 'st' | 'nd' | 'rd' = 'th';
-    
-      if (n % 10 === 1 && n % 100 !== 11) {
-        ord = 'st';
-      } else if (n % 10 === 2 && n % 100 !== 12) {
-        ord = 'nd';
-      } else if (n % 10 === 3 && n % 100 !== 13) {
-        ord = 'rd';
-      }
-    
-      return String(n) + ord;
-    }
-
-    if (localStorage.getItem('status') !== 'visited') {
-      countapi.update('wizzy-wig.netlify.app', countKey, 1)
-        .then((result) => {
-          const ordinal = getOrdinal(result.value);
-          console.log(`congratulations, you are the ${ordinal} unique visitor`);
-        }
-      )
-    } else {
-      countapi.get('wizzy-wig.netlify.app', countKey)
-        .then((result) => {
-          console.log(`this page has had ${result.value} unique visit${result.value !== 1 ? 's' : ''}`);
-        }
-      )
-    }
-
-    // uncomment to reset counter
-    // countapi.set('wizzy-wig.netlify.app', countKey, 0)
-    //   .then((result) => {
-    //     console.log(result);
-    //   }
-    // )
-
-    localStorage.setItem('status', 'visited');
-  }, [])
-  
   const handleClick = (e: Event) => {
     e.preventDefault();
   }
