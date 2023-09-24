09dd32531b675e84090eda4d604f044a16bb5d4a
Edward Do <edwardtdo@gmail.com>
2023-09-24 10:26:07 +1000

    file structure for vite updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+    port: 3000,
+  },
   base: '/',
   plugins: [react()],
   test: {
